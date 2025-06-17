72ff41d698a85419ace1e612b77ebaa1663be45a
Dero06440 <bigdero@gmail.com>
2025-06-17 10:47:41 +0200

    ajout lecture de message quand on clique

--- ViewModels/InboxViewModel.cs
+++ ViewModels/InboxViewModel.cs
@@ -1,17 +1,33 @@
 
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Windows.Input;
 using MauiMail.Models;
 using MauiMail.Services;
 
 namespace MauiMail.ViewModels
 {
-    public class InboxViewModel
+    public class InboxViewModel : INotifyPropertyChanged
     {
         public ObservableCollection<MauiMail.Models.EmailMessage> Messages { get; set; } = new();
         public ICommand RefreshCommand { get; }
+        private MauiMail.Models.EmailMessage? _selectedMessage;
+        public MauiMail.Models.EmailMessage? SelectedMessage
+        {
+            get => _selectedMessage;
+            set
+            {
+                if (_selectedMessage != value)
+                {
+                    _selectedMessage = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(SelectedMessage)));
+                }
+            }
+        }
 
         private readonly EmailService _emailService;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
 
         public InboxViewModel()
         {
